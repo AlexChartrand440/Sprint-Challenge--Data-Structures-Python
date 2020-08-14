5dff9020d1247a8fad6abbf54a89806df06201a8
Neytorokx <31908898+Neytoro@users.noreply.github.com>
2020-08-14 13:19:11 -0500

    Stretch

--- names/names.py
+++ names/names.py
@@ -32,10 +32,31 @@ for name_2 in names_2:
         duplicates.append(name_2);
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"BST runtime: {end_time - start_time} seconds. {len(duplicates)} duplicates.")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+start_time_2 = time.time()
+
+duplicates_2 = [];
+
+f = open('names_1.txt', 'r')
+names_3 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_4 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+for name in names_3:
+    if name in names_4:
+        duplicates_2.append(name);
+
+end_time_2 = time.time()
+
+# print (f"{len(duplicates_2)} duplicates:\n\n{', '.join(duplicates_2)}\n\n")
+print (f"List runtime: {end_time_2 - start_time_2} seconds. {len(duplicates_2)} duplicates.")
